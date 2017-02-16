043502310b0fa8bd56211219a5b135a65363fe30
Stian Vale <stian.westerhus.vale@gmail.com>
2017-02-15 23:49:32 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+function addProject(result){
+	console.log(result)
+	$("#project" + result["id"] + " .details").html(
+		"<img src="+result["image"]+" class='detailsImage'></img>"+
+		"<h3>"+result["title"]+"</h3>"+
+		"<h4>"+result["date"]+"</h4>"+
+		result["summary"]);
+}
+
 /*
  * Function that is called when the document is ready.
  */
@@ -27,6 +37,8 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber , addProject);
 }
 
 /*
@@ -35,4 +47,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", changeColor)
+}
+
+function changeColor(result){
+	console.log(result);
+	var colors = result["colors"]["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
